d9b28c7511a2fe940232c123c4584539167fe8b6
nick <nick@undeprecated.com>
2021-11-28 21:27:33 -0500

    Added CSS support

--- plugin/plugin.php
+++ plugin/plugin.php
@@ -15,9 +15,14 @@ define('${DEFINE_BASE}_VERSION', '${version}');
 define('${DEFINE_BASE}_BASE_FILE', plugin_basename(__FILE__));
 define('${DEFINE_BASE}_PLUGIN_URL', plugin_dir_url(__FILE__));
 
+/**
+ * Delete this ff you are NOT using Composer.
+ */
 require_once 'vendor/autoload.php';
 
 /**
  * Use files from ./includes directory.
  */
 ${namespace}\App::init();
+${namespace}\Ajax::init();
+${namespace}\Registers::init();
