5d3c3f75ebda59022bd29ea4fc6e8dc819ed94c9
Undeprecated <nick@undeprecated.com>
2026-03-22 14:54:38 -0400

    Fixes process not defined in production since this is a library build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
   plugins: [
     react({
       include: ['**/*.js', '**/*.jsx']
